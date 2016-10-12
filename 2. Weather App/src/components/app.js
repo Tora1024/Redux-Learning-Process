75e8f89c51ed5c8978c011b31844ef6837a84e4c
Hector Coto Rojas <heavenhell64@gmail.com>
2016-10-11 20:36:34 -0600

    Making progress here, learning a lot as well

--- 2. Weather App/src/components/app.js
+++ 2. Weather App/src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import SerchBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+      	<SerchBar />
+      </div>
     );
   }
 }
