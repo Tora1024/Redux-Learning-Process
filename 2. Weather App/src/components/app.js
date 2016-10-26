03dad9fd093f85d899b47f05a9d74e40e4aac7f1
Hector Coto Rojas <heavenhell64@gmail.com>
2016-10-25 23:36:23 -0600

    Starting again this  app, need to restart its understanding

--- 2. Weather App/src/components/app.js
+++ 2. Weather App/src/components/app.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 
-import SerchBar from '../containers/search_bar';
-import WeatherList from '../containers/weather_list';
-
 export default class App extends Component {
   render() {
     return (
-      <div>
-      	<SerchBar />
-      	<WeatherList />
-      </div>
+      <div>React simple starter</div>
     );
   }
 }
