89d0430e8de92f14c7269a380176efe5d20d97c7
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-01 10:18:16 -0600

    Now doing some  progress on our form, thought it still needs validation

--- 3. Simple Blog App/src/components/posts_new.js
+++ 3. Simple Blog App/src/components/posts_new.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPosts } from '../actions/index';
 
 class PostsNew extends Component {
 	render() {
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		console.log(this.props.createPosts);
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.props.createPosts)}>
 				<h3>Create A New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" { ...title }/>
 				</div>
 
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" { ...categories }/>
 				</div>
 
 				<div className="form-group">
 					<label>Content</label>
-					<textarea type="text" className="form-control" />
+					<textarea type="text" className="form-control" { ...content }/>
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
@@ -27,7 +30,10 @@ class PostsNew extends Component {
 	}
 }
 
+//connect: first argument is mapStateToProps, second is dispatchStateToProps
+//reduxForm: first is form configuration, second is mapStateToProps, third is dispatchStateToProps
+
 export default reduxForm({
 	form: 'PostsNewForm',
 	fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPosts })(PostsNew);
