bf5a350a55049acc62fbc3c618b491c8fe4495e9
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-01 18:01:00 -0600

    Fully validated and also with some other features to complete de UX

--- 3. Simple Blog App/src/components/posts_new.js
+++ 3. Simple Blog App/src/components/posts_new.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPosts } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsNew extends Component {
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
+	onSubmit(props) {
+		this.props.createPosts(props)
+			.then(() => {
+				//blog post have been created, navigate the user to the index
+				//We navigate by calling this.context.router.push with the 
+				//new path to navigate to
+				this.context.router.push('/');
+			});
+	}
+
 	render() {
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
 		return (
-			<form onSubmit={handleSubmit(this.props.createPosts)}>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<h3>Create A New Post</h3>
 				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
