83732cc4f55f1ab422d8060ab8212bc1836f141d
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-03 17:43:20 -0600

    Delete and go back features already built

--- 3. Simple Blog App/src/components/posts_show.js
+++ 3. Simple Blog App/src/components/posts_show.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
+import { deletePost } from '../actions/index';
 
 class PostsShow extends Component {
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
 	}
+
+	onPostDelete() {
+		this.props.deletePost(this.props.params.id)
+	}
+
 	render() {
 		const { post } = this.props;
 
@@ -16,7 +23,8 @@ class PostsShow extends Component {
 		} else {
 			return (
 				<div>
-					<h2>Showing Post Number: {this.props.params.id}</h2>
+					<Link to="/">Back To Index</Link>
+					<button onClick={this.onPostDelete.bind(this)} className="btn btn-danger pull-xs-right">Delete Post</button>
 					<h3>{post.title}</h3>
 					<h6>Categories: {post.categories}</h6>
 					<p>{post.content}</p>
@@ -30,4 +38,4 @@ function mapStateToProps(state) {
 	return ({ post: state.posts.post });
 }
 
-export default connect (mapStateToProps, { fetchPost })(PostsShow);
+export default connect (mapStateToProps, { fetchPost, deletePost })(PostsShow);
