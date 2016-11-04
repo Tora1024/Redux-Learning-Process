5a2087edc69012b4cd7342a0848627a5154a379c
Hector Coto Rojas <heavenhell64@gmail.com>
2016-11-04 11:26:57 -0600

    Gotta complete this

--- 4. Thunk Example App/src/components/app.js
+++ 4. Thunk Example App/src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+	componentWillMount(){
+		this.props.fetchUsers();
+	}
+
+	render() {
+		return (
+		  	<div>React simple starter</div>
+		);
+	}
 }
